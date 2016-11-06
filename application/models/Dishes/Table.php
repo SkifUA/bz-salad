647a2216e0b406ea57d3f39eb347cb6b822f0c66
skifua <kuzin.valeriy@gmail.com>
2016-11-06 08:27:13 +0200

    start

---
+++ application/models/Dishes/Table.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: valeriy
+ * Date: 06.11.16
+ * Time: 0:22
+ */
+
+namespace Application\Dishes;
+
+
+class Table extends \Bluz\Db\Table
+{
+    /**
+     * Table
+     *
+     * @var string
+     */
+    protected $table = 'dishes';
+}
---
+++ application/models/Menu/Table.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: valeriy
+ * Date: 05.11.16
+ * Time: 20:28
+ */
+
+namespace Application\Menu;
+
+
+class Table extends \Bluz\Db\Table
+{
+    /**
+     * Table
+     *
+     * @var string
+     */
+    protected $table = 'menu';
+}
